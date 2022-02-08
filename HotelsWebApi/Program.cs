76a0f351973388c18034dbd9d8a7d4d9078e23ec
Dimitrii Vinogradov <siteddv@gmail.com>
2022-02-09 05:34:35 +0600

    working on adding db

--- HotelsWebApi/Program.cs
+++ HotelsWebApi/Program.cs
@@ -1,9 +1,21 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<HotelDbContext>(options => 
+    options.UseSqlite(builder.Configuration.GetConnectionString("Sqlite"));
+
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment()){}
+
+{
+    using var scope = app.Services.CreateScope();
+    var db = scope.ServiceProvider.GetRequiredService<HotelDbContext>();
+    db.Database.EnsureCreated();
+}
 
 var hotels = new List<Hotel>();
 
-app.MapGet("/hotels", () => hotels);
+app.MapGet("/hotels", async (HotelDbContext dbContext) => await dbContext.Hotels.To);
 app.MapGet("/hotels/{id}", (int id) => hotels.FirstOrDefault(h => h.Id == id));
 app.MapPost("/hotels", (Hotel hotel) => hotels.Add(hotel));
 app.MapPut("/hotels", (Hotel hotel) =>
@@ -22,8 +34,13 @@ app.MapDelete("hotels/{id}", (int id) =>
 
     hotels.RemoveAt(index);
 });
-
 app.Run();
+
+public class HotelDbContext : DbContext
+{
+    public HotelDbContext(DbContextOptions<HotelDbContext> options) : base(options) {}
+    public DbSet<Hotel> Hotels => Set<Hotel>();
+}
 
 public class Hotel
 {
